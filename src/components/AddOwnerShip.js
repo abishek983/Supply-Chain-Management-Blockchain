f12f50b3e9d9f468622404839f8b4748334c4ed0
*abishek983* <*abishekwwe@gmail.com*>
2020-05-12 17:56:12 +0530

    product buttons added

--- src/components/AddOwnerShip.js
+++ src/components/AddOwnerShip.js
@@ -42,6 +42,7 @@ class AddOwnerShip extends Component {
                     <div>
                         <a href="/" className="btn btn-outline-primary btn-sm mx-1">Build Parts</a>
                         <a href="/getParts" className="btn btn-outline-primary btn-sm mx-2">Parts Details</a>
+                        <a href="/getProducts" className="btn btn-outline-primary btn-sm mx-1">Products Details</a>
                         <a href="/changeOwnerShip" className="btn btn-outline-primary btn-sm mx-1">Change Ownership</a>
                         <a href="/products" className="btn btn-outline-primary btn-sm mx-1">Build Product</a>
                     </div>
